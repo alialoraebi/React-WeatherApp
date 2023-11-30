74b28b579f78eaead802e24e1889e5fb1b438da6
Ali Al Oreabi <a.aloreabi2000@gmail.com>
2023-11-30 15:01:30 -0500

    worked on weatherDisplay

--- README.md
+++ README.md
@@ -6,5 +6,5 @@
 #### Testing with Postman
 ![Image description](./postman.png)
 
-#### This is how it looked like prior to adding css for styling
+#### This is how it looked like prior to adding CSS
 ![Image description](./pre-css.png)
--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 
 const WeatherDisplay = ({ weatherData }) => {
   const { main, weather, wind, name, dt } = weatherData;
 
   const temperatureCelsius = (main.temp - 273.15).toFixed(2);
-
-  const weatherCondition = weather[0].main;
   const weatherIcon = weather[0].icon;
-
+  const weatherDescription = weather[0].description;
   const windSpeedKmH = (wind.speed * 3.6).toFixed(2);
 
   const date = new Date(dt * 1000);
@@ -20,19 +18,21 @@ const WeatherDisplay = ({ weatherData }) => {
     <div className="weather-display-container">
       <div className="current-weather">
         <div className="date-location">
-          <p>{`${dayOfWeek}, ${month} ${day}`}</p>
-          <h2>{name}</h2>
+          <p className="date">{`${dayOfWeek}, ${month} ${day}`}</p>
+          <h2 className="location">{name}</h2>
         </div>
-        <div className="temperature">
+        <div className="temperature-section">
           <img src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`} alt="weather icon" />
-          <h1>{`${temperatureCelsius}°C`}</h1>
-          <p>{weatherCondition}</p>
+          <p className="temperature">{`${temperatureCelsius}°C`}</p>
+          <p className="weather-condition">{weatherDescription}</p>
         </div>
       </div>
       <div className="weather-details">
-        <p className="paragraph">Humidity: {main.humidity}%</p>
-        <p className="paragraph">Wind Speed: {windSpeedKmH} km/h</p>
-        <p className="paragraph">Pressure: {main.pressure} mb</p>
+        <p className="detail-item">Humidity: {main.humidity}%</p>
+        <p className="detail-item">Wind Speed: {windSpeedKmH} km/h</p>
+        <p className="detail-item">Pressure: {main.pressure} mb</p>
+        <p className="detail-item">Max Temp: {(main.temp_max - 273.15).toFixed(2)}°C</p>
+        <p className="detail-item">Min Temp: {(main.temp_min - 273.15).toFixed(2)}°C</p>
       </div>
     </div>
   );
